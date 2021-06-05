1a6eb8542e1f3dbc9185c3e8019f04950561fa75
D3viii <devinsdevs@gmail.com>
2021-06-05 14:58:54 -0400

    Initial Commit

---
+++ devinschatapp/devinschatappApp.swift
@@ -0,0 +1,17 @@
+//
+//  devinschatappApp.swift
+//  devinschatapp
+//
+//  Created by Devin Carr on 6/5/21.
+//
+
+import SwiftUI
+
+@main
+struct devinschatappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
